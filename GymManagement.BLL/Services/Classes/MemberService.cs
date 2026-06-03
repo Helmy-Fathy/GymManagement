1ba321b0e881df8b0076ae5b7b2c2042180b02f0
Helmy-Fathy <7elmyfathy@gmail.com>
2026-06-03 07:20:10 +0300

    Index Action Service

---
+++ GymManagement.BLL/Services/Classes/MemberService.cs
@@ -0,0 +1,41 @@
+﻿using GymManagement.BLL.Services.Interfaces;
+using GymManagement.BLL.ViewModels.MemberViewModels;
+using GymManagement.DAL.Data.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.BLL.Services.Classes
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IGenericRepository<Member> _memberRepository;
+
+        public MemberService(IGenericRepository<Member> memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+        public async Task<IEnumerable<MemberViewModel>> GetAllMembersAsync(CancellationToken ct = default)
+        {
+            var members = await _memberRepository.GetAllAsync(ct: ct);
+            if (!members.Any()) return [];
+
+            var membersViewModel = members.Select(m => new MemberViewModel()
+            {
+                Name = m.Name,
+                Email = m.Email,
+                Gender = m.Gender.ToString(),
+                Phone = m.PhoneNumber,
+                Photo = m.Photo,
+                Id = m.Id
+            });
+
+
+            return membersViewModel;
+
+        }
+    }
+}
---
+++ GymManagement/Controllers/MembersController.cs
@@ -0,0 +1,50 @@
+﻿using GymManagement.DAL.Data.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.PL.Controllers
+{
+    public class MembersController : Controller
+    {
+        private readonly IGenericRepository<Member> _memberRepository;
+
+        public MembersController(IGenericRepository<Member> memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+
+        // GET BaseUrl/Members/Index
+        // Index - List all members
+
+        //GET BaseUrl/Members/MemberDeatails/{id}
+        //MemberDetails - show one member's details
+
+        //GET BaseUrl/Members/HealthRecordDeatails/{id}
+        //HealthRecordDeatails - show one member's HealthRecord details
+
+        #region Create Member 
+        //GET BaseUrl/Members/Create
+        //Create - show empty form
+
+        //POST BaseUrl/Members/Create {member}
+        //Create - Submit form 
+        #endregion
+
+        #region Edit Member
+        //GET BaseUrl/Members/Edit/{id}
+        //Edit - Display edit form
+
+        //POST BaseUrl/Members/Edit {member}
+        //Edit - Submit form  
+        #endregion
+
+        #region Delete Member 
+        //GET BaseUrl/Members/Delete/{id}
+        //Delete - show confirmation form
+
+        //POST BaseUrl/Members/DeleteConfirmed/{id}
+        //DeleteConfirmed - Submit form  
+        #endregion
+
+    }
+}
