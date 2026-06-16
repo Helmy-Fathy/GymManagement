a1bed6ea38431f90b0e8ddd10fda5156313e8229
Helmy-Fathy <7elmyfathy@gmail.com>
2026-06-16 07:27:56 +0300

    Plan Module

---
+++ GymManagement.BLL/Services/Classes/PlanService.cs
@@ -0,0 +1,105 @@
+﻿using GymManagement.BLL.Services.Interfaces;
+using GymManagement.BLL.ViewModels.PlanViewModels;
+using GymManagement.DAL.Data.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.BLL.Services.Classes
+{
+    public class PlanService : IPlanService
+    {
+        private readonly IGenericRepository<Plan> _planRepository;
+        private readonly IGenericRepository<Membership> _membershipRepository;
+
+        public PlanService(IGenericRepository<Plan> planRepository, IGenericRepository<Membership> membershipRepository)
+        {
+            _planRepository = planRepository;
+            _membershipRepository = membershipRepository;
+        }
+        public async Task<IEnumerable<PlanViewModel>> GetAllPlansAsync(CancellationToken ct = default)
+        {
+            var plans = await _planRepository.GetAllAsync(ct: ct);
+            return plans.Select(p => new PlanViewModel()
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                DurationDays = p.DurationDays,
+                IsActive = p.IsActive,
+                Price = p.Price,
+            });
+        }
+
+        public async Task<PlanViewModel?> GetPlanByIdAsync(int planId, CancellationToken ct = default)
+        {
+            var plan = await _planRepository.GetByIdAsync(planId, ct);
+            if (plan == null)
+                return null;
+            else
+                return new PlanViewModel()
+                {
+                    Name = plan.Name,
+                    Description = plan.Description,
+                    Price = plan.Price,
+                    DurationDays = plan.DurationDays,
+                    IsActive = plan.IsActive,
+                };
+        }
+
+        public async Task<UpdatePlanViewModel?> GetPlanToUpdateAsync(int planId, CancellationToken ct = default)
+        {
+            var plan = await _planRepository.GetByIdAsync(planId, ct);
+            if (plan is null || !plan.IsActive) return null;
+            if (await HasActiveMembershipsAsync(planId, ct))
+                return null;
+            else
+                return new UpdatePlanViewModel()
+                {
+                    PlanName = plan.Name,
+                    Description = plan.Description,
+                    Price = plan.Price,
+                    DurationDays = plan.DurationDays
+                };
+        }
+
+        public async Task<bool> UpdatePlanAsync(int planId, UpdatePlanViewModel model, CancellationToken ct = default)
+        {
+            var plan = await _planRepository.GetByIdAsync(planId, ct);
+            if (plan is null) return false;
+            if (await HasActiveMembershipsAsync(planId, ct))
+                return false;
+
+            plan.DurationDays = model.DurationDays;
+            plan.Price = model.Price;
+            plan.Description = model.Description;
+            plan.UpdatedAt = DateTime.Now;
+            var result = await _planRepository.UpdateAsync(plan, ct);
+            return result > 0;
+        }
+
+        public async Task<bool> ToggleActivationAsync(int planId, CancellationToken ct = default)
+        {
+            var plan = await _planRepository.GetByIdAsync(planId, ct);
+            if (plan is null) return false;
+
+            if (plan.IsActive && await HasActiveMembershipsAsync(planId, ct))
+                return false;
+
+            plan.IsActive = !plan.IsActive;
+            plan.UpdatedAt = DateTime.Now;
+            var result = await _planRepository.UpdateAsync(plan, ct);
+            return result > 0;
+        }
+
+
+
+        private async Task<bool> HasActiveMembershipsAsync(int planId, CancellationToken ct = default)
+        {
+            return await _membershipRepository.AnyAsync(m => m.PlanId == planId && m.EndDate > DateTime.Now, ct);
+        }
+    }
+}
--- GymManagement/Controllers/PlansController.cs
+++ GymManagement/Controllers/PlansController.cs
@@ -5,31 +5,75 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using GymManagement.DAL.Data.Models;
+using GymManagement.BLL.Services.Interfaces;
+using GymManagement.BLL.ViewModels.PlanViewModels;
 
 namespace GymManagement.Controllers
 {
     public class PlansController : Controller
     {
-        private readonly IGenericRepository<Plan> _planRepository;
+        private readonly IPlanService _planService;
 
-        public PlansController(IGenericRepository<Plan> planRepository)
+        public PlansController(IPlanService planService)
         {
-            _planRepository = planRepository;
+            _planService = planService;
         }
 
         public async Task<IActionResult> Index(CancellationToken ct)
+            => View(await _planService.GetAllPlansAsync(ct));
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id, CancellationToken ct)
         {
-            var plans = await _planRepository.GetAllAsync(ct: ct);
-            return View(plans);
+            var plan = await _planService.GetPlanByIdAsync(id, ct);
+            if (plan is null)
+            {
+                TempData["Errormessage"] = "Plan Not found";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+                return View(plan);
         }
 
-        public async Task<IActionResult> Details(int id , CancellationToken ct) 
-        { 
-            var plan = await _planRepository.GetByIdAsync(id,ct);
-            if(plan is null)
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id, CancellationToken ct)
+        {
+            var plan = await _planService.GetPlanToUpdateAsync(id, ct);
+            if (plan is null)
+            {
+                TempData["Errormessage"] = "Plan cannot be edited (not found, inactive, or has active memberships)";
                 return RedirectToAction(nameof(Index));
-            else 
+            }
+            else
                 return View(plan);
-        } 
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, UpdatePlanViewModel model, CancellationToken ct)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var result = await _planService.UpdatePlanAsync(id, model, ct);
+            if (result)
+                TempData["SuccsessMessage"] = "Plan updated Successfully";
+            else
+                TempData["ErrorMessage"] = "Plan failed to update";
+
+            return RedirectToAction(nameof(Index));  
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Activate(int id, CancellationToken ct)
+        {
+            var result = await _planService.ToggleActivationAsync(id, ct);
+            if (result)
+                TempData["SuccsessMessage"] = "Plan status changed";
+            else
+                TempData["ErrorMessage"] = "Failed to Toggle Plan status";
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
