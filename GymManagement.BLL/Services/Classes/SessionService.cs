4e4b90397f43247c6b2b78626a927fb49672f448
Helmy-Fathy <7elmyfathy@gmail.com>
2026-06-19 07:41:28 +0300

    Session Module - IndexAction

---
+++ GymManagement.BLL/Services/Classes/SessionService.cs
@@ -0,0 +1,48 @@
+﻿using GymManagement.BLL.Services.Interfaces;
+using GymManagement.BLL.ViewModels.SessionViewModels;
+using GymManagement.DAL.Data.Models;
+using GymManagement.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.BLL.Services.Classes
+{
+    public class SessionService : ISessionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SessionService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IEnumerable<SessionViewModel>?> GetAllSessionsAsync(CancellationToken ct = default)
+        {
+            var sessionRepo = _unitOfWork.SessionRepository;
+            var sessions = await sessionRepo.GetAllSesssionsWithTrainerAndCategory(ct);
+            if (sessions == null || !sessions.Any()) return null;
+
+            var mappedSessions = sessions.Select(s => new SessionViewModel()
+            {
+                Id = s.Id,
+                Capacity = s.Capacity,
+                CategoryName = s.Category.CategryName,
+                TrainerName = s.Trainer.Name,
+                Description = s.Description,
+                EndDate = s.EndDate,
+                StartDate = s.StartDate,
+            });
+
+            foreach (var session in mappedSessions)
+            {
+                session.AvailableSlots = session.Capacity - await sessionRepo.GetCountOfBookedSlotsAsync(session.Id, ct);
+                //N + 1 Problem
+            }
+
+            return mappedSessions;
+
+        }
+    }
+}
---
+++ GymManagement.DAL/Repositories/Interfaces/ISessionRepository.cs
@@ -0,0 +1,16 @@
+﻿using GymManagement.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.DAL.Repositories.Interfaces
+{
+    public interface ISessionRepository : IGenericRepository<Session>
+    {
+        Task<IEnumerable<Session>> GetAllSesssionsWithTrainerAndCategory(CancellationToken ct = default);
+
+        Task<int> GetCountOfBookedSlotsAsync(int sessionId, CancellationToken ct = default);
+    }
+}
